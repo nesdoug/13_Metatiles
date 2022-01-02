a67c5fca510391feeb827a943b6d7a3db876c8e4
nesdoug <dougfraker@gmail.com>
2022-01-02 16:07:23 -0500

    update
    update

--- metatiles.c
+++ metatiles.c
@@ -29,7 +29,6 @@ void main (void) {
 	bank_spr(1);
 
 	set_vram_buffer(); // do at least once, sets a pointer to a buffer
-	clear_vram_buffer();
 
 	load_room();
 	
@@ -44,8 +43,6 @@ void main (void) {
 		
 		pad1 = pad_poll(0); // read the first controller
 		
-		clear_vram_buffer(); // do at the beginning of each frame
-		
 		movement();
 		draw_sprites();
 	}
@@ -59,11 +56,10 @@ void load_room(void){
 	set_mt_pointer(metatiles1);
 	for(y=0; ;y+=0x20){
 		for(x=0; ;x+=0x20){
-			clear_vram_buffer(); // do each frame, and before putting anything in the buffer
 			address = get_ppu_addr(0, x, y);
 			index = (y & 0xf0) + (x >> 4);
 			buffer_4_mt(address, index); // ppu_address, index to the data
-			flush_vram_update_nmi();
+			flush_vram_update2();
 			if (x == 0xe0) break;
 		}
 		if (y == 0xe0) break;
